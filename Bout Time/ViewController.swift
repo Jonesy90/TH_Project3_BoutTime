78da11e249ac2b679f6c73b3c20bfd9e4bd871bc
Michael Jones <michael.jones90@me.com>
2017-07-02 17:42:00 +0100

    - Assigned the rest of the background images to the buttons.

--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -15,10 +15,23 @@ class ViewController: UIViewController {
     //List of buttons
     @IBOutlet weak var downFullButton: UIButton!
     
+    @IBOutlet weak var secondLabelHalfUp: UIButton!
+    @IBOutlet weak var secondLabelHalfDown: UIButton!
+    
+    @IBOutlet weak var thirdLabelHalfUp: UIButton!
+    @IBOutlet weak var thirdLabelHalfDown: UIButton!
+    
+    @IBOutlet weak var upFullButton: UIButton!
+    
+    
     
     
     //Giving each image provided it's own UIImage.
     let fullDownImage = UIImage(named: "down_full.png")
+    let downHalfImage = UIImage(named: "down_half.png")
+    let upHalfImage = UIImage(named: "up_half.png")
+    let fullUpImage = UIImage(named: "up_full.png")
+    
     
     
     
@@ -27,11 +40,20 @@ class ViewController: UIViewController {
     func loadButtonBackgroundImage() {
         //allocating the images to each button.
         downFullButton.setBackgroundImage(fullDownImage, for: .normal)
-        
+        secondLabelHalfUp.setBackgroundImage(upHalfImage, for: .normal)
+        secondLabelHalfDown.setBackgroundImage(downHalfImage, for: .normal)
+        thirdLabelHalfUp.setBackgroundImage(upHalfImage, for: .normal)
+        thirdLabelHalfDown.setBackgroundImage(downHalfImage, for: .normal)
+        upFullButton.setBackgroundImage(fullUpImage, for: .normal)
         
         
         //Removing the title from each button.
         downFullButton.setTitle("", for: .normal)
+        secondLabelHalfUp.setTitle("", for: .normal)
+        secondLabelHalfDown.setTitle("", for: .normal)
+        thirdLabelHalfUp.setTitle("", for: .normal)
+        thirdLabelHalfDown.setTitle("", for: .normal)
+        upFullButton.setTitle("", for: .normal)
     }
     
     
