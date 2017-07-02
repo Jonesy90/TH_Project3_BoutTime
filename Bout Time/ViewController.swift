241597c3f726457df52181a2c3628a0f6731b5c4
Michael Jones <michael.jones90@me.com>
2017-07-02 17:26:25 +0100

    - Created a function that would load the images onto the background of the buttons. - Organised the UI of the app to match the mock ups provided.

--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    //List of buttons
+    @IBOutlet weak var downFullButton: UIButton!
+    
+    
+    
+    //Giving each image provided it's own UIImage.
+    let fullDownImage = UIImage(named: "down_full.png")
+    
+    
+    
+    
+    //Giving the buttons the correct background images.
+    func loadButtonBackgroundImage() {
+        //allocating the images to each button.
+        downFullButton.setBackgroundImage(fullDownImage, for: .normal)
+        
+        
+        
+        //Removing the title from each button.
+        downFullButton.setTitle("", for: .normal)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadButtonBackgroundImage()
     }
 
     override func didReceiveMemoryWarning() {
